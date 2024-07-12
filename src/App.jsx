3f112dd99bc5616e7cc1440c377d66c1dce1645b
bati-squadra <bautista@squadralabs.com>
2024-07-12 10:17:08 -0300

    estilos-navbar-hover

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 //HOOKS
 import usePopup from "./hooks/usePopup";
+import Footer from '../components/Footer';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Router>
         <div>
           <Home showPopup={showPopup} hidePopup={hidePopup} />
+          <Footer />
         </div>
       </Router>
       <Popup />
